25d581d364717727ba30e61ea34d8f14c359650d
Your Name <michaelgolenia1@gmail.com>
2022-11-22 20:51:24 -0300

    Mysql setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,12 @@
+-- create the database hbnb_dev_db in your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- create a new user with the all the permises for the database hbnb_dev_db
+
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- SELECT permises for the database performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEGES;
