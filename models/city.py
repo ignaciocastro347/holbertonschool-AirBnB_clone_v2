290776bb4b7547fabf710965bf0d12c2125c738b
Your Name <michaelgolenia1@gmail.com>
2022-11-26 18:22:56 -0300

    Relationship error fixed --- :)

--- models/city.py
+++ models/city.py
@@ -11,6 +11,4 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__="cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id",
-		ondelete="CASCADE"), nullable=False)
-    state = relationship("State", back_populates="cities")
+    state_id = Column(String(60), ForeignKey("states.id", ondelete="CASCADE"), nullable=False)
--- models/state.py
+++ models/state.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
 from os import getenv
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     
     # file ? for FileStorage or db for DBStorage
@@ -13,7 +14,7 @@ class State(BaseModel):
         
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship("City", back_populates="state", cascade="all, delete")
+        cities = relationship("City", backref="states", cascade="all, delete")
 
     elif getenv("HBNB_TYPE_STORAGE") == "file":
         
