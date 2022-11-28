89acaadc62f25e2beab43f7b4e1c8a95bb3805c8
MichaelTwG <michaelgolenia1@gmail.com>
2022-11-28 11:15:13 -0300

    Task: 0 --- Create a web application that return Hello HBNB!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ Module 0-hello_route """
 
-from flask import Flask;
+from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def index():
-    return "Hello HBNB! HOLA"
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
