0e5364e41fc44e8d30e41d2a0a539c17c2ad5987
MichaelTwG <michaelgolenia1@gmail.com>
2022-11-28 13:12:55 -0300

    Task 2:    routing variables     ---     escape

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Module 1-hbnb_route """
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c(text):
+    return "C {}".format(escape(text.replace("_", " ")))
+    
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
