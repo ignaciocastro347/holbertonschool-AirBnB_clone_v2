26ea6c42d774f18b4cff1f9a965254ab895feaca
Jeremias Araujo <jerearaujo03@gmail.com>
2022-11-29 20:30:29 -0300

    Fix pycodestyle in 0_hello_route file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,9 +4,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def home():
-  return f'Hello HBNB!'
+    """ Function that returns a message """
+    return f'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
